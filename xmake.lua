b2efed9a2b18041ea3f8176458a0d9c390d4c6e7
dofes <91889957+dofes@users.noreply.github.com>
2023-12-27 20:04:18 +0800

    feat: update to 1.20.51

--- xmake.lua
+++ xmake.lua
@@ -1,36 +1,11 @@
 add_repositories("liteldev-repo https://github.com/LiteLDev/xmake-repo.git")
-
-add_requires("levilamina develop")
+add_requires("levilamina 0.2.1")
 
 if not has_config("vs_runtime") then
     set_runtimes("MD")
 end
 
-package("levilamina")
-    add_urls("https://github.com/LiteLDev/LeviLamina.git")
-
-    -- Dependencies from xmake-repo.
-    add_deps("entt 3.12.2")
-    add_deps("fmt 10.1.1")
-    add_deps("gsl 4.0.0")
-    add_deps("leveldb 1.23")
-    add_deps("magic_enum 0.9.0")
-    add_deps("nlohmann_json 3.11.2")
-    add_deps("rapidjson 1.1.0")
-
-    -- Dependencies from liteldev-repo.
-    add_deps("bdslibrary 1.20.41.02")
-    add_deps("ctre 3.8.1")
-    add_deps("pcg_cpp 1.0.0")
-    add_deps("pfr 2.1.1")
-    add_deps("preloader 1.3.0")
-    add_deps("symbolprovider 1.1.0")
-
-    on_install(function (package)
-        import("package.tools.xmake").install(package)
-    end)
-
-target("LeviOptimize") -- Change this to your plugin name.
+target("LeviOptimize")
     add_cxflags(
         "/utf-8",
         "/permissive-",
@@ -43,19 +18,27 @@ target("LeviOptimize") -- Change this to your plugin name.
         "/w44738",
         "/w45204"
     )
+    add_cxxflags(
+        "-Wno-c++2b-extensions",
+        "-Wno-microsoft-cast",
+        "-Wno-pragma-system-header-outside-header",
+        {tools = {"clang_cl"}}
+    )
     add_defines(
         "_AMD64_",
         "_CRT_SECURE_NO_WARNINGS",
         "_ENABLE_CONSTEXPR_MUTEX_CONSTRUCTOR",
         "NOMINMAX",
         "UNICODE",
-        "WIN32_LEAN_AND_MEAN"
+        "WIN32_LEAN_AND_MEAN",
+        "ENTT_PACKED_PAGE=128",
+        "_HAS_CXX23=1"
     )
     add_files(
         "src/**.cpp"
     )
     add_includedirs(
-        "include"
+        "src","include"
     )
     add_packages(
         "levilamina"
@@ -68,9 +51,12 @@ target("LeviOptimize") -- Change this to your plugin name.
     add_shflags(
         "/DELAYLOAD:bedrock_server.dll"
     )
+    add_undefines(
+        "_DEBUG"
+    )
     set_exceptions("none")
     set_kind("shared")
-    set_languages("c++23")
+    set_languages("cxx20")
     set_strip("all")
 
     after_build(function (target)
