f9a725bd9e34dfd578620463aa6bf5badf3e4051
dofes <91889957+dofes@users.noreply.github.com>
2023-12-27 20:06:20 +0800

    feat: add RandomTickOpt

---
+++ src/features/RandomTickOpt.cpp
@@ -0,0 +1,40 @@
+#include "features.h"
+
+#include "ll/api/memory/Hook.h"
+
+#include "mc/world/level/block/Block.h"
+#include "mc/world/level/block/utils/StaticVanillaBlocks.h"
+
+namespace lo::random_tick_opt {
+
+LL_TYPED_INSTANCE_HOOK(
+    BlockShouldRandomTickHook,
+    ll::memory::HookPriority::Normal,
+    Block,
+    &Block::shouldRandomTick,
+    bool
+) {
+    if (this == StaticVanillaBlocks::mAir || this == StaticVanillaBlocks::mStone || this == StaticVanillaBlocks::mWater
+        || this == StaticVanillaBlocks::mNetherrack || this == StaticVanillaBlocks::mDeepslate
+        || this == StaticVanillaBlocks::mBedrock) {
+        return false;
+    }
+    return origin();
+}
+
+struct RandomTickOpt::Impl {
+    ll::memory::HookRegistrar<BlockShouldRandomTickHook> r;
+};
+
+void RandomTickOpt::call(bool enable) {
+    if (enable) {
+        impl = std::make_unique<Impl>();
+    } else {
+        impl.reset();
+    }
+};
+
+RandomTickOpt::RandomTickOpt()  = default;
+RandomTickOpt::~RandomTickOpt() = default;
+
+} // namespace lo::random_tick_opt
