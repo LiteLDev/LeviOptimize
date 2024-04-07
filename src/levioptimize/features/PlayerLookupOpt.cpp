f9df78683dd41fb782c319cc9bf53b2e22b3b5b4
Dofes <91889957+Dofes@users.noreply.github.com>
2024-04-08 03:27:02 +0800

    fix: fix playerlookup crash

--- src/levioptimize/features/PlayerLookupOpt.cpp
+++ src/levioptimize/features/PlayerLookupOpt.cpp
@@ -1,12 +1,17 @@
 #include "features.h"
 
+#include "mc\deps\core\mce\UUID.h"
+
 #include "ll/api/memory/Hook.h"
 #include "ll/api/service/Bedrock.h"
 #include "mc/certificates/Certificate.h"
 #include "mc/certificates/ExtendedCertificate.h"
+#include "mc/entity/gamerefs_entity/EntityRegistry.h"
 #include "mc/network/ServerNetworkHandler.h"
 #include "mc/server/ServerInstance.h"
+#include "mc/server/ServerLevel.h"
 #include "mc/world/ActorUniqueID.h"
+#include "mc/world/actor/common/GameplayUserManager.h"
 #include "mc/world/level/Level.h"
 
 
@@ -29,7 +34,6 @@ LL_TYPE_INSTANCE_HOOK(
     if (it != playersCacheByUUID.end()) {
         return it->second;
     } else if (auto pl = origin(uuid); pl) {
-        playersCacheByUUID.emplace(uuid, pl);
         return pl;
     } else {
         return nullptr;
@@ -48,7 +52,6 @@ LL_TYPE_INSTANCE_HOOK(
     if (it != playersCacheByUniqueID.end()) {
         return it->second;
     } else if (auto pl = origin(entityID); pl) {
-        playersCacheByUniqueID.emplace(entityID.id, pl);
         return pl;
     } else {
         return nullptr;
@@ -67,92 +70,49 @@ LL_TYPE_INSTANCE_HOOK(
     if (it != playersCacheByName.end()) {
         return it->second;
     } else if (auto pl = origin(name); pl) {
-        playersCacheByName.emplace(name, pl);
         return pl;
     } else {
         return nullptr;
     }
 }
 
+#include "mc/world/actor/Actor.h"
 
 LL_TYPE_INSTANCE_HOOK(
-    PlayerHook,
+    ServerLevel_onGameplayUserAdded,
     ll::memory::HookPriority::Normal,
-    Player,
-    "??0Player@@QEAA@AEAVLevel@@AEAVPacketSender@@W4GameType@@_NAEBVNetworkIdentifier@@W4SubClientId@@VUUID@mce@@AEBV?$"
-    "basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@7V?$unique_ptr@VCertificate@@U?$default_delete@"
-    "VCertificate@@@std@@@9@AEAVEntityContext@@77@Z",
-    Player*,
-    class Level&                       level,
-    class PacketSender&                sender,
-    ::GameType                         gameType,
-    bool                               bool1,
-    class NetworkIdentifier const&     networkIdentifier,
-    ::SubClientId                      subClientId,
-    class mce::UUID const&             uuid,
-    std::string const&                 str1,
-    std::string const&                 str2,
-    std::unique_ptr<class Certificate> certificate,
-    class EntityContext&               entityContext,
-    std::string const&                 platformOfflineId,
-    std::string const&                 platformDeviceId
-
+    ServerLevel,
+    &ServerLevel::_onGameplayUserAdded,
+    void,
+    class EntityContext& entity
 ) {
-    auto res = origin(
-        level,
-        sender,
-        gameType,
-        bool1,
-        networkIdentifier,
-        subClientId,
-        uuid,
-        str1,
-        str2,
-        std::move(certificate),
-        entityContext,
-        platformOfflineId,
-        platformDeviceId
-    );
-    playersCacheByName.emplace(getName(), res);
-    playersCacheByUUID.emplace(uuid, res);
-    return res;
-}
-
-LL_TYPE_INSTANCE_HOOK(
-    ServerNetworkHandlerCreateNewPlayerHook,
-    ll::memory::HookPriority::Normal,
-    ServerNetworkHandler,
-    &ServerNetworkHandler::_createNewPlayer,
-    ServerPlayer&,
-    class NetworkIdentifier const&          networkIdentifier,
-    class SubClientConnectionRequest const& request,
-    ::SubClientId                           subClientId
-) {
-    auto& res = origin(networkIdentifier, request, subClientId);
-    playersCacheByUniqueID.emplace(res.getOrCreateUniqueID().id, &res);
-    return res;
-}
-
-LL_TYPE_INSTANCE_HOOK(
-    ServerNetworkHandlerTrytLoadPlayerHook,
-    ll::memory::HookPriority::Normal,
-    ServerNetworkHandler,
-    &ServerNetworkHandler::trytLoadPlayer,
-    bool,
-    class ServerPlayer&            player,
-    class ConnectionRequest const& connectionRequest
-) {
-    auto res = origin(player, connectionRequest);
-    playersCacheByUniqueID.emplace(player.getOrCreateUniqueID().id, &player);
-    return res;
+    auto ac = Actor::tryGetFromEntity(entity, true);
+    // it->mEntity.try
+    if (ac) {
+        playersCacheByUUID.emplace(((Player*)ac)->getUuid(), (Player*)ac);
+        playersCacheByUniqueID.emplace(ac->getOrCreateUniqueID().id, (Player*)ac);
+        playersCacheByName.emplace(((Player*)ac)->getName(), (Player*)ac);
+    }
+    return origin(entity);
 }
 
 
-LL_TYPE_INSTANCE_HOOK(PlayerLeaveEventHook, HookPriority::Normal, ServerPlayer, &ServerPlayer::disconnect, void) {
-    playersCacheByUUID.erase(getUuid());
-    playersCacheByUniqueID.erase(getOrCreateUniqueID().id);
-    playersCacheByName.erase(getName());
-    origin();
+LL_TYPE_INSTANCE_HOOK(
+    ServerLevel_onGameplayUserRemoved,
+    ll::memory::HookPriority::Normal,
+    ServerLevel,
+    &ServerLevel::_onGameplayUserRemoved,
+    void,
+    class EntityContext& entity
+) {
+    auto ac = Actor::tryGetFromEntity(entity, true);
+    // it->mEntity.try
+    if (ac) {
+        playersCacheByUUID.erase(((Player*)ac)->getUuid());
+        playersCacheByUniqueID.erase(ac->getOrCreateUniqueID().id);
+        playersCacheByName.erase(((Player*)ac)->getName());
+    }
+    return origin(entity);
 }
 
 struct PlayerLookupOpt::Impl {
@@ -160,16 +120,28 @@ struct PlayerLookupOpt::Impl {
         LevelQueryPlayer,
         LevelQueryPlayer1,
         LevelQueryPlayer2,
-        PlayerHook,
-        ServerNetworkHandlerCreateNewPlayerHook,
-        ServerNetworkHandlerTrytLoadPlayerHook,
-        PlayerLeaveEventHook>
+        ServerLevel_onGameplayUserAdded,
+        ServerLevel_onGameplayUserRemoved>
         r;
 };
 
 void PlayerLookupOpt::call(bool enable) {
     if (enable) {
-        if (!impl) impl = std::make_unique<Impl>();
+        if (!impl) {
+            impl       = std::make_unique<Impl>();
+            auto level = ll::service::getLevel();
+            if (level) {
+                level->forEachUser([&](EntityContext& pl) {
+                    auto ac = Actor::tryGetFromEntity(pl, true);
+                    if (ac) {
+                        playersCacheByUUID.emplace(((Player*)ac)->getUuid(), (Player*)ac);
+                        playersCacheByUniqueID.emplace(ac->getOrCreateUniqueID().id, (Player*)ac);
+                        playersCacheByName.emplace(((Player*)ac)->getName(), (Player*)ac);
+                    }
+                    return true;
+                });
+            }
+        }
     } else {
         impl.reset();
         playersCacheByUUID.clear();
@@ -182,3 +154,56 @@ PlayerLookupOpt::PlayerLookupOpt()  = default;
 PlayerLookupOpt::~PlayerLookupOpt() = default;
 
 } // namespace lo::player_lookup_opt
+
+
+// #include "ll/api/event/Cancellable.h"
+// #include "ll/api/event/EventBus.h"
+// #include "ll/api/event/player/PlayerJoinEvent.h"
+// #include "ll/api/event/player/PlayerLeaveEvent.h"
+// #include "ll/api/memory/Hook.h"
+// #include "ll/api/schedule/Scheduler.h"
+// #include "ll/api/service/Bedrock.h"
+// #include "ll/api/utils/ErrorUtils.h"
+// #include "mc/certificates/Certificate.h"
+// #include "mc/network/ServerNetworkHandler.h"
+// #include "mc/network/packet/TextPacket.h"
+// #include "mc/server/ServerInstance.h"
+// #include "mc/world/ActorRuntimeID.h"
+// #include "mc/world/events/ServerInstanceEventCoordinator.h"
+// #include "mc/world/level/Level.h"
+// #include <thread>
+
+// LL_AUTO_TYPE_INSTANCE_HOOK(
+//     EventTestH,
+//     ll::memory::HookPriority::Normal,
+//     ServerInstanceEventCoordinator,
+//     &ServerInstanceEventCoordinator::sendServerThreadStarted,
+//     void,
+//     ::ServerInstance& ins
+// ) {
+//     origin(ins);
+//     using namespace ll::event;
+//     auto& bus = ll::event::EventBus::getInstance();
+//     bus.emplaceListener<PlayerJoinEvent>([](PlayerJoinEvent& ev) {
+//         std::thread t([uuid_ = ev.self().getUuid(), eid = ev.self().getOrCreateUniqueID().id] {
+//             std::vector<int64> total_it1 = {0, 0};
+//             std::vector<int64> total_it3 = {0, 0};
+//             std::vector<int64> total_it2 = {0, 0};
+//             while (true) {
+//                 auto start = std::chrono::high_resolution_clock::now();
+//                 auto pl    = ll::service::getLevel()->getPlayer(uuid_);
+//                 auto end   = std::chrono::high_resolution_clock::now();
+//                 if (!pl) {
+//                     return;
+//                 }
+//                 std::cout << "player lookup by uuid: "
+//                           << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << "us"
+//                           << std::endl;
+//                 auto pkt = TextPacket::createJukeboxPopup("I LOVE U", {});
+//                 pl->sendNetworkPacket(pkt);
+//                 std::this_thread::sleep_for(std::chrono::microseconds(1));
+//             }
+//         });
+//         t.detach();
+//     });
+// }
